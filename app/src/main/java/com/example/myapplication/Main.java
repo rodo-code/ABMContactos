e14345228d1ba7f0a3670ec953b454cd883d5b3b
Alexander1119 <49416284+Alexander1119@users.noreply.github.com>
2019-08-29 21:07:44 -0400

    metodo primo

--- app/src/main/java/com/example/myapplication/Main.java
+++ app/src/main/java/com/example/myapplication/Main.java
@@ -1,4 +1,14 @@
 class Main{
+	
+	public bool primo(int numero){
+
+	if(numero==1) return false;	
+	for(int i=2;i<numero;i++){
+		if(numero%i==0) return false;		
+	}
+	return true;
+	}
+
 	int fibo(int n){
 		if(n==0 or n==1){
 			return 1;
